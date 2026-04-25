835271f9183f95305f13f1a46b2fcf91d52ea554
VladKuli <goldydefault@gmail.com>
2026-04-25 16:58:06 +0300

    Add main

---
+++ src/main/java/insurance/quote/calculator/InsuranceQuoteCalculatorApplication.java
@@ -0,0 +1,13 @@
+package insurance.quote.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceQuoteCalculatorApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(InsuranceQuoteCalculatorApplication.class, args);
+    }
+
+}
